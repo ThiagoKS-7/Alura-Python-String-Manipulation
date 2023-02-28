54b1451f98249ed5a6be3b417eae05681459bbba
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2023-02-28 14:26:33 -0300

    #2-release - validações dos tipos de URL que a classe aceita

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-
 class QueryToDict:
     def __init__(self, url):
+        self.__set_values__(url)
+        
+    def __set_values__(self, url):
         self.url = url
-        self.param_index = self.url.find("?")+1
+        if type(url) is str:
+            self.param_index = self.url.find("?")+1
     
     def __str__(self):
         return  str(self.start())
-        
-    def validate_protocol(self, protocol:list[str]) -> ValueError:
-        if all(s not in self.url for s in protocol):
-            raise ValueError("Invalid URL. Must be HTTP or HTTPS")
-        
+            
     def check_params_in_query(self) -> dict:
-        self.validate_protocol(["http://", "https://"])
+        if all(s not in self.url for s in ["http://", "https://"]):
+            return ValueError(f"Error - Invalid URL '{self.url}'. Must be HTTP or HTTPS")
         if self.param_index:
             return {
                 i.split('=')[0]:i.split("=")[1]
                 for i in self.url[self.param_index:].split("&")
             }
-        return {"Error":"Não tem parâmetro"}
+        return ValueError(f"Error - No query params found in '{self.url}'")
         
     def start(self) -> dict:
-        if len(self.url.strip()) > 0:
+        if type(self.url) is str and len(self.url.strip()) > 0:
             return self.check_params_in_query()
-        raise ValueError("Invalid URL. Must not be empty")
+        return ValueError(f"Error - Invalid URL '{self.url} {'- ' + str(type(self.url)) if type(self.url) is not str else ''}'. Must be a string and not empty")
         
 
 def main():
     print(QueryToDict("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"))
+    # print(QueryToDict("https://bytebank.com"))
+    # print(QueryToDict(None))
+    # print(QueryToDict(""))
+    # print(QueryToDict("     "))
+    # print(QueryToDict(2))
+    # print(QueryToDict("bytebank"))
+
 
 if __name__ == '__main__':
     main()
