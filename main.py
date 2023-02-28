b2381ca44508f1c180abe6bc1b785d15189220ee
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2023-02-28 13:19:29 -0300

    #1-release: implementando função

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+def query_to_dict(url:str) -> str:
+    if len(url.strip()) > 0:
+        if all(s not in url for s in ["http://", "https://"]):
+            raise ValueError("Invalid URL. Must be HTTP or HTTPS")
+        param_index = url.find("?")+1
+        if param_index:
+            return {
+                i.split('=')[0]:i.split("=")[1]
+                for i in url[param_index:].split("&")
+            }
+        return "Não tem parâmetro"
+    raise ValueError("Invalid URL. Must not be empty")
+
 def main():
-    url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100" 
-    print(url)
+    print(query_to_dict("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"))
 
 if __name__ == '__main__':
     main()
